e0b84b1553bf4b00073a824a4b2e09c06f757a4a
Munene-Kariuki <denismunene999@gmail.com>
2022-05-15 16:33:40 +0300

    add answers as variables to javscript for manipulation

--- assets/js/styles.js
+++ assets/js/styles.js
@@ -14,7 +14,11 @@
 function onSubmit () {
   var score = 0;
   var noOfQuestions = 5;
-  var answersArray = ['b', 'c', 'd', 'a', 'b']
+  var answersArray = ['b', 'c', 'd', 'a', 'b'];
 
-  
+  var q1 = document.forms ['quiz'] ['q1'].value;
+  var q2 = document.forms ['quiz'] ['q2'].value;
+  var q3 = document.forms ['quiz'] ['q3'].value;
+  var q4 = document.forms ['quiz'] ['q4'].value;
+  var q5 = document.forms ['quiz'] ['q5'].value;
 }
