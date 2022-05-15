a451a63ed1fdbf76b18a8eb03c6283d80ab8282b
Munene-Kariuki <denismunene999@gmail.com>
2022-05-15 15:32:37 +0300

    restructure the form on the html document

--- assets/js/styles.js
+++ assets/js/styles.js
@@ -1,9 +1,8 @@
 function check_answer() {
-  var form_no = document.getElementById()
-  var answer = document.forms[0,4];
+  var answer = document.forms[0];
   var text = '';
   var i;
-
+  
   for (i=0; i < answer.length; i++) {
     if (answer[i].checked){
       text += answer[i].value
