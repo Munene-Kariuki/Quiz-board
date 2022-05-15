16acf5c95f4f73adc46db21657c421b637028349
Munene-Kariuki <denismunene999@gmail.com>
2022-05-15 16:54:46 +0300

    create a for loop to compute the score

--- assets/js/styles.js
+++ assets/js/styles.js
@@ -14,11 +14,19 @@
 function onSubmit () {
   var score = 0;
   var noOfQuestions = 5;
-  var answersArray = ['b', 'c', 'd', 'a', 'b'];
+  var answersArray = ['B', 'C', 'D', 'A', 'B'];
 
   var q1 = document.forms ['quiz'] ['q1'].value;
   var q2 = document.forms ['quiz'] ['q2'].value;
   var q3 = document.forms ['quiz'] ['q3'].value;
   var q4 = document.forms ['quiz'] ['q4'].value;
   var q5 = document.forms ['quiz'] ['q5'].value;
+
+  for (var i = 1; i <= noOfQuestions; i++) {
+    if (eval( 'q' + i) == answersArray[i - 1]) {
+      score++;
+      alert (score)
+    }
+
+  }
 }
