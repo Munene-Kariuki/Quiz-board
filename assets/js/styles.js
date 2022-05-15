e42251008fe8b21d3f57a7ad3e55254d67d05028
Munene-Kariuki <denismunene999@gmail.com>
2022-05-15 16:20:41 +0300

    create onSubmit function

--- assets/js/styles.js
+++ assets/js/styles.js
@@ -1,12 +1,20 @@
-function check_answer() {
-  var answer = document.forms[0];
-  var text = '';
-  var i;
+// function check_answer() {
+//   var answer = document.forms[0];
+//   var text = '';
+//   var i;
+
+//   for (i=0; i < answer.length; i++) {
+//     if (answer[i].checked){
+//       text += answer[i].value
+//     }
+//   }
+//   console.log(text)
+// }
+
+function onSubmit () {
+  var score = 0;
+  var noOfQuestions = 5;
+  var answersArray = ['b', 'c', 'd', 'a', 'b']
+
   
-  for (i=0; i < answer.length; i++) {
-    if (answer[i].checked){
-      text += answer[i].value
-    }
-  }
-  console.log(text)
 }
